c054fc27da7962c63b30bc876e0947b8d2a3cc99
kecoma1 <47478959+kecoma1@users.noreply.github.com>
2020-11-03 11:55:06 +0100

    Changed asm

--- P2/programa.asm
+++ P2/programa.asm
@@ -42,12 +42,22 @@ executes_branch:
   lw $t5, 20($zero) # lw $r13, 16($r0) -> r13 = 32
   lw $t6, 20($zero) # lw $r14, 20($r0) -> r14 = 32
 executes_branch2:
+  add $t4, $t4, $t4
   lw $t3, 8($zero)  # lw $r11, 8($r0)  -> r11 = 4
   beq $t3, $t2, executes_branch2 # DOESN'T JUMP
   lw $t1, 20($zero)  # lw $r9,  0($r0)  -> r9  = 32
   lw $t2, 20($zero)  # lw $r10, 4($r0)  -> r10 = 32
   lw $t3, 20($zero)  # lw $r11, 8($r0)  -> r11 = 32 
-  lw $t4, 20($zero) # lw $r12, 12($r0) -> r12 = 32 
   lw $t5, 20($zero) # lw $r13, 16($r0) -> r13 = 32
   lw $t6, 20($zero) # lw $r14, 20($r0) -> r14 = 32
+  lw $t1, 0($zero)  # lw $r9,  0($r0)  -> r9  = 1
+  lw $t2, 4($zero)  # lw $r10, 4($r0)  -> r10 = 2
+  beq $t1, $t2, executes_branch2 # DOESN'T JUMP
+  beq $t1, $t1, executes_branch2 # JUMP
+
+  
+  
+
+  
+  
   
