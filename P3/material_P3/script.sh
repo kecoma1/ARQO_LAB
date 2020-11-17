ff34a3d13dcebb9479e64aca3f9e25cbfb63a681
kecoma1 <kecoma2@gmail.com>
2020-11-17 12:59:32 +0100

    Added code related with the script
    Co-authored-by: Miguel Herrera Martinez <miguel.herreramartinez@estudiante.uam.es>

--- P3/material_P3/script.sh
+++ P3/material_P3/script.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-$MAX = 256 #17168
-$MIN = 1 #16144
-for i in $(seq 6 16 $MAX); do # Esta mal
-    echo "SIZE: $i"
-    ./slow $i
+Nfinal=256 #17168
+Ninicio=0 #16144
+Npaso=16
+
+rm -f raw_output.log just_times.log
+for i in $(seq $Ninicio $Npaso $(($Nfinal-$Npaso)) ); do
+
+    for n in $(seq 1 1 15); do
+        echo -e -n "SLOW\t$i\t" >> "raw_output.log"
+        ./slow $i | grep "time" >> "raw_output.log"
+
+        $slow_1 += slow_1
+        $slow_2
+
+        echo -e -n "SLOW\t$(($i+$Npaso))\t" >> "raw_output.log"
+        ./slow $i+$Npaso | grep "time" >> "raw_output.log"
+        echo -e -n "FAST\t$i\t" >> "raw_output.log"
+        ./fast $i | grep "time" >> "raw_output.log"
+        echo -e -n "FAST\t$(($i+$Npaso))\t" >> "raw_output.log"
+        ./fast $i+$Npaso | grep "time" >> "raw_output.log"
+    done
+
+    echo 
 done
